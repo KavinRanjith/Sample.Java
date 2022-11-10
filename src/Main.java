52795f3a11bbaa1eb61c72c8cd4d45ab64bfd190
KAVIN C <kavin.c@sunwaretechnologies.com>
2022-11-10 16:10:54 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+//Increment and Decrement
+
+//Increment and Decrement Operator
+public class Main
+{
+    public static void main(String[] args)
+    {
+        postIncrement();
+        preIncrement();
+        postDecrement();
+        preDecrement();
     }
-    //this is sample for git
+    // Post Increment Operator
+    public static void postIncrement(){
+        int i = 0;
+        System.out.println(i++);
+    }
+
+    // Pre Increment Operator
+    public static void preIncrement(){
+        int i = 0;
+        System.out.println(++i);
+    }
+
+    //Post decrement Operator
+    public static void  postDecrement(){
+        int i = 0;
+        System.out.println(i--);
+    }
+
+    //Pre decrement Operator
+    public static void preDecrement() {
+        int i = 0;
+        System.out.println(--i);
+    }
 }
