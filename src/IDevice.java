b446849877ad47c51cc8600d9a93e51cc76279ff
KAVIN C <kavin.c@sunwaretechnologies.com>
2022-11-16 13:37:01 +0530

    interfaceExample

--- src/IDevice.java
+++
@@ -1,7 +0,0 @@
-//advance oops abstraction concept
-public interface IDevice {   //parent class(Inheritance)
-    //we can declare method  without public,private,protect etc.,
-    String getDevice();    // abstract method1
-
-    void deviceCalled();   // abstract method2
-}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,60 @@
-public class Main {
-    public static void main(String[] args) {
-        IDevice device = new mobileDevice();      //parent class with new object method...
-        IDevice device1 = new LaptopDevice();
-        System.out.println(device.getDevice());   //return method...
-        System.out.println(device1.getDevice());
-        device.deviceCalled();                    //O/p...
-        device1.deviceCalled();
+//Example 1
+interface P
+{
+    String p = "pppp";
+    String methodP();
+}
+
+interface Q extends P
+{
+    String q = "QQQQ";
+    String methodQ();
+}
+
+class R implements P, Q
+{
+    public String methodP()
+    {
+        return p+q;
     }
+    public String methodQ()
+    {
+        return q+p;
+    }
+}
+
+//Example 2
+class A implements B
+{
+    public int methodB(int i)
+    {
+        return i =+ i*i;
+    }
+}
+interface B
+{
+    int methodB(int i);
+}
+
+//Example 3:How do you access interface field ‘i’ in the below code?
+class X
+{
+    interface Y
+    {
+        int i = 111;
+    }
+}
+public class Main {
+    public static void main(String[] args)
+    {
+     R r = new R();  //Example 1
+        System.out.println(r.methodP());
+        System.out.println(r.methodQ());
+
+     B b = new A();  //Example 2
+        System.out.println(b.methodB(2));
+
+        System.out.println(X.Y.i);  //Example 3
+    }
+
 }
